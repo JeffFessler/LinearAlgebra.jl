45d8bd20ecdb3a9313776d4b13294f43f83871ac
Jishnu Bhattacharya <jishnub.github@gmail.com>
2026-04-12 10:25:42 +0530

    Move bounds-check to the top-level functions

--- src/sparse_interface.jl
+++ src/sparse_interface.jl
@@ -12,11 +12,17 @@ Return an iterable collection of the row indices of the nonzero entries in colum
 The returned indices should be sorted.
 """
 function nzrows(A, col)
+    require_one_based_indexing(A)
     checkbounds(A, axes(A,1), col)
-    axes(A, 1)
+    _nzrows(A, col)
 end
 
-nzrows(A::AbstractVector, col) = col == 1 ? nonzeroinds(A) : throw(BoundsError(A, (":", col)))
+_nzrows(A, col) = axes(A, 1)
+
+function nzrows(A::AbstractVector, col)
+    require_one_based_indexing(A)
+    col == 1 ? nonzeroinds(A) : throw(BoundsError(A, (":", col)))
+end
 
 """
     nzcols(A, row::Integer)
@@ -25,24 +31,20 @@ Return an iterable collection of the column indices of the nonzero entries in ro
 The returned indices should be sorted.
 """
 function nzcols(A, row)
+    require_one_based_indexing(A)
     checkbounds(A, row, axes(A,2))
-    axes(A, 2)
+    _nzcols(A, row)
 end
+
+_nzcols(A, row) = axes(A, 2)
 
 # special matrices
 
-function nzrows(D::Diagonal, col)
-    checkbounds(D, axes(D,1), col)
-    return col:col
-end
+_nzrows(::Diagonal, col) = col:col
 
-function nzcols(D::Diagonal, row)
-    checkbounds(D, row, axes(D,2))
-    return row:row
-end
+_nzcols(::Diagonal, row) = row:row
 
-function nzrows(B::Bidiagonal, col)
-    checkbounds(B, axes(B,1), col)
+function _nzrows(B::Bidiagonal, col)
     if B.uplo == 'U'
         return max(1, col-1):col
     else
@@ -50,8 +52,7 @@ function nzrows(B::Bidiagonal, col)
     end
 end
 
-function nzcols(B::Bidiagonal, row)
-    checkbounds(B, row, axes(B,2))
+function _nzcols(B::Bidiagonal, row)
     if B.uplo == 'U'
         return row:min(length(B.dv), row+1)
     else
@@ -59,53 +60,45 @@ function nzcols(B::Bidiagonal, row)
     end
 end
 
-function nzrows(T::Union{Tridiagonal, SymTridiagonal}, col)
-    checkbounds(T, axes(T,1), col)
+function _nzrows(T::Union{Tridiagonal, SymTridiagonal}, col)
     return max(1, col-1):min(length(T.d), col+1)
 end
 
-function nzcols(T::Union{Tridiagonal, SymTridiagonal}, row)
-    checkbounds(T, row, axes(T,2))
+function _nzcols(T::Union{Tridiagonal, SymTridiagonal}, row)
     return max(1, row-1):min(length(T.d), row+1)
 end
 
-function nzrows(U::UpperOrUnitUpperTriangular, col)
-    checkbounds(U, axes(U,1), col)
+function _nzrows(U::UpperOrUnitUpperTriangular, col)
     nzrows_parent = nzrows(parent(U), col)
     ind_parent = findlast(<=(col), nzrows_parent)
     return @view nzrows_parent[begin:(isnothing(ind_parent) ? (begin-1) : ind_parent)]
 end
 
-function nzcols(U::UpperOrUnitUpperTriangular, row)
-    checkbounds(U, row, axes(U,2))
+function _nzcols(U::UpperOrUnitUpperTriangular, row)
     nzcols_parent = nzcols(parent(U), row)
     ind_parent = findfirst(>=(row), nzcols_parent)
     return @view nzcols_parent[(isnothing(ind_parent) ? (end+1) : ind_parent):end]
 end
 
-function nzrows(L::LowerOrUnitLowerTriangular, col)
-    checkbounds(L, axes(L,1), col)
+function _nzrows(L::LowerOrUnitLowerTriangular, col)
     nzrows_parent = nzrows(parent(L), col)
     inds_parent = findfirst(>=(col), nzrows_parent)
     return @view nzrows_parent[(isnothing(inds_parent) ? (end+1) : inds_parent):end]
 end
 
-function nzcols(L::LowerOrUnitLowerTriangular, row)
-    checkbounds(L, row, axes(L,2))
+function _nzcols(L::LowerOrUnitLowerTriangular, row)
     nzcols_parent = nzcols(parent(L), row)
     ind_parent = findlast(<=(row), nzcols_parent)
     return @view nzcols_parent[begin:(isnothing(ind_parent) ? (begin-1) : ind_parent)]
 end
 
-function nzrows(H::UpperHessenberg, col)
-    checkbounds(H, axes(H,1), col)
+function _nzrows(H::UpperHessenberg, col)
     nzrows_parent = nzrows(parent(H), col)
     inds_parent = findlast(<=(col+1), nzrows_parent)
     return @view nzrows_parent[begin:(isnothing(inds_parent) ? (begin-1) : inds_parent)]
 end
 
-function nzcols(H::UpperHessenberg, row)
-    checkbounds(H, row, axes(H,2))
+function _nzcols(H::UpperHessenberg, row)
     nzcols_parent = nzcols(parent(H), row)
     ind_parent = findfirst(>=(row-1), nzcols_parent)
     return @view nzcols_parent[(isnothing(ind_parent) ? (end+1) : ind_parent):end]
